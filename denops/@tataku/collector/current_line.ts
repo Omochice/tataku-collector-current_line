ab8ceb5f137570703c5f595bc5b3da3e405ca25c
Omochice <mochice.mls.ntl@gmail.com>
2022-12-30 20:46:00 +0900

    :muscle: Use interface

--- denops/@tataku/collector/current_line.ts
+++ denops/@tataku/collector/current_line.ts
@@ -1,11 +1,12 @@
 import { Denops } from "https://deno.land/x/denops_std@v3.8.1/mod.ts";
 import * as fn from "https://deno.land/x/denops_std@v3.8.1/function/mod.ts";
+import { Collector } from "https://raw.githubusercontent.com/Omochice/tataku.vim/master/denops/tataku/interface.ts";
 
-export default class {
-  constructor(protected option: Record<string, unknown>) {
+export default class implements Collector {
+  constructor(_option: Record<string, unknown>) {
   }
 
-  run(denops: Denops) {
+  async run(denops: Denops) {
     return [await fn.getline(denops, ".")];
   }
 }
