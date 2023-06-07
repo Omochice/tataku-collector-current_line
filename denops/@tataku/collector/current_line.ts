f9ef6ce38fd3966cfc52cbca8532185adf0f1357
Omochice <mochice.mls.ntl@gmail.com>
2023-06-07 21:27:22 +0900

    :package: update deps

--- denops/@tataku/collector/current_line.ts
+++ denops/@tataku/collector/current_line.ts
@@ -1,5 +1,5 @@
-import { Denops } from "https://deno.land/x/denops_std@v3.8.1/mod.ts";
-import * as fn from "https://deno.land/x/denops_std@v3.8.1/function/mod.ts";
+import { Denops } from "https://deno.land/x/denops_std@v5.0.0/mod.ts";
+import * as fn from "https://deno.land/x/denops_std@v5.0.0/function/mod.ts";
 import { Collector } from "https://raw.githubusercontent.com/Omochice/tataku.vim/master/denops/tataku/interface.ts";
 
 export default class implements Collector {
