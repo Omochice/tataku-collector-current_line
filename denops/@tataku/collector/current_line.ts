6e28430c65cebaf26018e78315a88e72769d8a3e
Omochice <mochice.mls.ntl@gmail.com>
2025-01-30 21:29:29 +0900

    refactor: use type check

--- denops/@tataku/collector/current_line.ts
+++ denops/@tataku/collector/current_line.ts
@@ -1,7 +1,8 @@
 import { Denops } from "jsr:@denops/std@7.4.0";
 import * as fn from "jsr:@denops/std@7.4.0/function";
+import type { CollectorFactory } from "jsr:@omochice/tataku-vim@1.1.0";
 
-const collector = (denops: Denops, _option: unknown) => {
+const collector: CollectorFactory = (denops: Denops) => {
   return new ReadableStream<string[]>({
     start: async (controller: ReadableStreamDefaultController<string[]>) => {
       controller.enqueue([await fn.getline(denops, ".")]);
